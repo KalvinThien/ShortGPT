03250f05027407a0818de28fbd7a908aa1b36619
KalvinThien <120565705+KalvinThien@users.noreply.github.com>
2023-07-26 08:19:05 +0700

    Update edge_voice.py

--- shortGPT/audio/edge_voice.py
+++ shortGPT/audio/edge_voice.py
@@ -1,12 +1,37 @@
-from your_edge_tts_api_module import generateVoice
+import edge_tts
 from shortGPT.audio.voice_module import VoiceModule
+import asyncio
+
+voices = {
+    'vi': {'Male': 'vi-VN-NamMinhNeural', 'Female': 'vi-VN-HoaiMyNeural'},
+    'en': {'Male': 'en-US-SteffanNeural', 'Female': 'en-US-AriaNeural'},
+    'ms': {'Male': 'ms-MY-OsmanNeural', 'Female': 'ms-MY-YasminNeural'},
+    'lo': {'Male': 'lo-LA-SouvanhNeural', 'Female': 'lo-LA-VilayNeural'},
+    'th': {'Male': 'th-TH-NiwatNeural', 'Female': 'th-TH-PremwadeeNeural'},
+    'my': {'Male': 'my-MM-ThihaNeural', 'Female': 'my-MM-NilarNeural'},
+    # thêm những ngôn ngữ khác tại đây
+}
+
+# Tạo ánh xạ từ tên ngôn ngữ đầy đủ sang mã ngôn ngữ
+language_names = {
+    "Vietnamese": "vi",
+    "English": "en",
+    "Malaysia": "ms",
+    "Lao": "lo",
+    "ThaiLan": "th",
+    "Myanmar": "my",
+}
 
 class EdgeTTSVoiceModule(VoiceModule):
-    def __init__(self, api_key, voiceName):
-        self.api_key = api_key
-        self.voiceName = voiceName
+    def __init__(self, voiceName, language):
+        language_code = language_names[language]
+        self.voiceName = voices[language_code][voiceName]
         super().__init__()
 
-    def generate_voice(self, text, outputfile):
-        file_path = generateVoice(text=text, voice=self.voiceName, fileName=outputfile, api_key=self.api_key)
-        return file_path
+    async def generate_voice(self, text, outputfile):
+        communicate = edge_tts.Communicate(text, self.voiceName)
+        with open(outputfile, "wb") as file:
+            async for chunk in communicate.stream():
+                if chunk["type"] == "audio":
+                    file.write(chunk["data"])
+        return outputfile
